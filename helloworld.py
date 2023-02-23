55099570bca40f3ec05e55c088243c756ea4fb9d
DevOps-Team3-2022 <116635128+DevOps-Team3-2022@users.noreply.github.com>
2023-02-24 02:38:55 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,6 @@
-def helloworld():
-    print("Hello World!")
-
-helloworld()
+def hello_world(name):
+    if len(name) == 0:
+        return "Input string is empty!"        
+    elif len(name) > 10:
+        return "Input string too long!"
+    return f"Hello {name}! Welcome to Hello World File!"
