cfeb239986a6aac9f9bff894bc07271be3d7d25d
DevOps-Team3-2022 <116635128+DevOps-Team3-2022@users.noreply.github.com>
2023-02-24 02:47:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ from hello_world import hello_world
 import pytest
 
 def test_test():
-  result1 = helloworld("Test")
+  result1 = hello_world("Test")
   assert result1 == "Hello Test! Welcome to Hello World File!"
 
 def test_toolong():
-  result2 = helloworld(".........................")
+  result2 = hello_world(".........................")
   assert result2 == "Input string too long!"
 
 def test_empty():
-  result3 = helloworld("")
+  result3 = hello_world("")
   assert result3 == "Input string is empty!"
